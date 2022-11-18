3a355379b15ec3b32d17c043cd9900810c30adf0
LinuxDecoded <ajshrmaofficial@gmail.com>
2022-11-18 15:11:51 +0530

    replaced box with king

--- Components/King.js
+++ Components/King.js
@@ -1,11 +1,12 @@
 import React from "react"
-import { View } from "react-native"
+import { Image, View } from "react-native"
 import stylesheet from "./stylesheet"
 
 const King = ({kingPosY}) => {
     let kingHeigh = 50
     return(
-        <View style={[stylesheet.king, {bottom: kingPosY-kingHeigh}]}/>
+        // <View style={[stylesheet.king, {bottom: kingPosY-kingHeigh}]}/>
+        <Image source={require('../assets/king.png')} style={[stylesheet.king, {bottom: kingPosY-kingHeigh}]}/>
     )
 }
 
