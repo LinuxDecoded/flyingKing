6a4b68f4990f1dd978648a8bd24980e3608658bf
LinuxDecoded <ajshrmaofficial@gmail.com>
2022-11-22 22:38:27 +0530

    removed redundant code

--- Components/King.js
+++ Components/King.js
@@ -5,7 +5,6 @@ import stylesheet from "./stylesheet"
 const King = ({kingPosY}) => {
     let kingHeigh = 50
     return(
-        // <View style={[stylesheet.king, {bottom: kingPosY-kingHeigh}]}/>
         <Image source={require('../assets/king.png')} style={[stylesheet.king, {bottom: kingPosY-kingHeigh}]}/>
     )
 }
